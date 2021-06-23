f85216b3d4ab4a23e8071014749d895d8abd37d6
yacinebest <yacine.besta@gmail.com>
2021-06-23 22:55:52 +0100

    adding different components,services,guard...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { HomeComponent } from './components/user/home/home.component';
+import { DetailComponent } from './components/user/detail/detail.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { UserListComponent } from './components/admin/user-list/user-list.component';
+import { ProductListComponent } from './components/admin/product-list/product-list.component';
+import { UserTemplateComponent } from './components/template/user-template/user-template.component';
+import { AdminTemplateComponent } from './components/template/admin-template/admin-template.component';
+import { NotFoundComponent } from './components/error/not-found/not-found.component';
+import { UnauthorizedComponent } from './components/error/unauthorized/unauthorized.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    HomeComponent,
+    DetailComponent,
+    DashboardComponent,
+    UserListComponent,
+    ProductListComponent,
+    UserTemplateComponent,
+    AdminTemplateComponent,
+    NotFoundComponent,
+    UnauthorizedComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/template/user-template/user-template.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-template',
+  templateUrl: './user-template.component.html',
+  styleUrls: ['./user-template.component.css']
+})
+export class UserTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
